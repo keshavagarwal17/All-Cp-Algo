12757de47500de236dff42bd6a6c4be3a0ebab3b
AgarwalReema <72161271+AgarwalReema@users.noreply.github.com>
2020-10-01 01:51:45 +0530

    Create if_else.cpp

---
+++ if_else.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n=2;
+  if(n>0)
+  cout<<"n is positive";
+  else if(n==0)
+  cout<<"n is zero";
+  else
+  cout<<"n is negative";
+}
